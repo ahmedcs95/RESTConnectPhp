f375d0dedb909b98aa30be45da5438efbdeda86e
Ahmed.a.elfattah <ahmed.a.elfattah88@gmail.com>
2017-08-23 18:00:10 +0200

    Initial commit

---
+++ app/src/main/java/net/ekhtar/restconnectphp/MainActivity.java
@@ -0,0 +1,58 @@
+package net.ekhtar.restconnectphp;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.textView);
+
+
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://developerhendy.16mb.com/hello.php");
+                    // URL url = new URL("http://ekhtar.net/hello.php");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStreamReader inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+                    BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+                    final String hello=bufferedReader.readLine();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(hello);
+                        }
+                    });
+
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    Toast.makeText(getApplicationContext(),e1.getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+
+        Thread thread=new Thread(runnable);
+        thread.start();
+    }
+}
